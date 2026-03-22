09aa11862b81f60e43aa3b8789c3f05f59c5e55a
Peroxidize <marionemmanuelbuhat@gmail.com>
2026-03-22 15:56:32 +0800

    nvim: add keymap for :q!

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,6 +43,7 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "<leader>v", ":vsp<CR>")
 vim.keymap.set("n", "<leader>hh", ":vertical resize +15<CR>")
 vim.keymap.set("n", "<leader>ll", ":vertical resize -15<CR>")
+vim.keymap.set("n", "<leader><leader>q", ":quit!<CR>")
 
 vim.diagnostic.config({ virtual_text = true })
 
